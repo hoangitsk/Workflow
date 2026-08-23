4a5dda539f85d2f4ce9cdc5b2422965f0b3609fc
hoangitsk <hoangitsk@users.noreply.github.com>
2026-08-24 00:01:53 +0700

    Auto deploy at 2026-08-24 00:01:53

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['192.168.1.14'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  }
 };
 
 export default nextConfig;
